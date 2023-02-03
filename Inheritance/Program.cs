37e594357a6f847f16788e7382aa9fbba5affa4c
jose valdez <josezepedavaldez30@gmail.com>
2023-02-02 19:03:00 -0800

    initial commit

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,11 +1,41 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Inheritance
 {
-    class Program
+     class Program
     {
         static void Main(string[] args)
+
         {
+            var myBird = new Bird();
+            myBird.WingColor = "blue";
+            myBird.CanFly = true;
+            myBird.WillMigrate = true;
+            myBird.BeakLength = 3.5;
+
+
+            var lizard = new Reptile
+            {
+                IsColdBlooded = true,
+                IsScaly = true,
+                Habitat = "swamp",
+                CanGrowTail = true,
+
+
+
+            };
+            var myAnimals = new Animal[] { myBird, lizard };
+            foreach(var animal in myAnimals) 
+            {
+               Console.WriteLine($"Alive:{animal.IsAlive}");
+               Console.WriteLine($"Age:{animal.Age} years old");
+               Console.WriteLine($"It has {animal.LegCount} legs");
+               Console.WriteLine($"It lives by :{animal.LandSeaAir}");
+              Console.WriteLine($"");
+
+            }
+
             // TODO Be sure to follow best practice when creating your classes
 
             // Create a class Animal
---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Reptile : Animal
+    {
+        public Reptile()
+        {
+            IsAlive = true;
+            LegCount = 4 ;
+            LandSeaAir = "land" ;
+            Age = 3 ;
+        }
+        public bool IsColdBlooded { get; set; }
+        public bool IsScaly { get; set; }
+        public string Habitat { get; set; }
+        public bool CanGrowTail { get; set; }
+    }
+}
